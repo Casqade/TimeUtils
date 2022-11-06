f2861d8fd762a8d1c21d90b3028e9b6938a40240
casqade <ivamspasibo@gmail.com>
2022-11-07 01:48:32 +0300

    upd: get rid of platform-dependent macros, use timespec as underlying Duration type

--- include/TimeUtils/Duration.hpp
+++ include/TimeUtils/Duration.hpp
@@ -2,29 +2,7 @@
 
 #include <TimeUtils/TimeUtils_export.hpp>
 
-#include <cstdint>
-
-#if defined(__APPLE__) || defined(__MACH__)
-  static_assert ( true, "TimeUtils is not compatible with OS X" );
-
-#elif defined(_WIN32)
-  #include <windows.h>
-
-  #ifndef TIME_UTILS_WIN
-    #define TIME_UTILS_WIN
-  #endif
-
-#elif defined(__linux__)
-  #include <time.h>
-
-  #ifndef TIME_UTILS_LIN
-    #define TIME_UTILS_LIN
-  #endif
-
-#else
-  static_assert ( true, "TimeUtils is not compatible with your OS" );
-
-#endif
+#include <ctime>
 
 
 namespace TimeUtils
@@ -33,35 +11,29 @@ namespace TimeUtils
 class Duration;
 
 TIMEUTILS_EXPORT Duration Now();
-TIMEUTILS_EXPORT bool Sleep( const Duration& duration );
-TIMEUTILS_EXPORT bool SleepUntil( const Duration& timeStamp );
+TIMEUTILS_EXPORT int Sleep( const Duration& duration );
+TIMEUTILS_EXPORT int SleepUntil( const Duration& timeStamp );
 
 TIMEUTILS_EXPORT void TimePeriodInit();
 TIMEUTILS_EXPORT void TimePeriodDeinit();
 
 class TIMEUTILS_EXPORT Duration
 {
-  int64_t mSec {};
-  int64_t mNSec {};
+  timespec mTime {};
 
 public:
   Duration() = default;
+
+  Duration( const timespec& time );
   Duration( const double time );
-  Duration( const int64_t sec,
-            const int64_t nsec );
+
+  Duration( const time_t sec,
+            const long nsec );
 
   bool sleep() const;
   Duration now();
 
-#if defined (TIME_UTILS_WIN)
-  Duration( const LARGE_INTEGER& time );
-  operator LARGE_INTEGER () const;
-
-#elif defined (TIME_UTILS_LIN)
-  Duration( const timespec& time );
-  operator timespec () const;
-
-#endif
+  explicit operator timespec () const;
   explicit operator double () const;
   explicit operator float () const;
 
--- src/Duration.cpp
+++ src/Duration.cpp
@@ -2,63 +2,47 @@
 
 #include <cmath>
 
+#if defined(_WIN32)
+  #include <windows.h>
+#endif
+
 
 namespace TimeUtils
 {
 
-const static int64_t NSEC_IN_SEC = 1'000'000'000;
+const static long NSEC_IN_SEC = 1'000'000'000;
 
 Duration
 Now()
 {
-#if defined TIME_UTILS_WIN
-  LARGE_INTEGER currentTime;
-  QueryPerformanceCounter( &currentTime );
-
-#elif defined TIME_UTILS_LIN
   timespec currentTime;
   clock_gettime( CLOCK_MONOTONIC, &currentTime );
-
-#endif
 
   return currentTime;
 }
 
-bool
+int
 Sleep( const Duration& duration )
 {
-#if defined TIME_UTILS_WIN
-  const Duration timestamp = TimeUtils::Now() + duration;
-  return SleepUntil( timestamp );
+  const auto timeToSleep = static_cast <timespec> (duration);
 
-#elif defined TIME_UTILS_LIN
-  timespec timeToSleep = duration;
-  clock_nanosleep( CLOCK_MONOTONIC, 0, &timeToSleep, NULL );
-
-  return true;
-#endif
+  return clock_nanosleep( CLOCK_MONOTONIC, 0,
+                          &timeToSleep, NULL );
 }
 
-bool
+int
 SleepUntil( const Duration& timestamp )
 {
-#if defined TIME_UTILS_WIN
-  while ( TimeUtils::Now() < timestamp )
-    ::Sleep(1); // It's the most accurate solution as we can get
+  const auto timeToSleep = static_cast <timespec> (timestamp);
 
-#elif defined TIME_UTILS_LIN
-  timespec timeToSleep = timestamp;
-  clock_nanosleep( CLOCK_MONOTONIC, TIMER_ABSTIME, &timeToSleep, NULL );
-
-#endif
-
-  return true;
+  return clock_nanosleep( CLOCK_MONOTONIC, TIMER_ABSTIME,
+                          &timeToSleep, NULL );
 }
 
 void
 TimePeriodInit()
 {
-#if defined TIME_UTILS_WIN
+#if defined(_WIN32)
   timeBeginPeriod(1);
 #endif
 }
@@ -66,21 +50,25 @@ TimePeriodInit()
 void
 TimePeriodDeinit()
 {
-#if defined TIME_UTILS_WIN
+#if defined(_WIN32)
   timeEndPeriod(1);
 #endif
 }
 
 
-Duration::Duration( const double time )
-  : mSec(static_cast <int64_t> ( std::trunc(time) ))
-  , mNSec(static_cast <int64_t> ( time * NSEC_IN_SEC ) % NSEC_IN_SEC)
+Duration::Duration( const struct timespec& time )
+  : mTime{time}
 {}
 
-Duration::Duration( const int64_t sec,
-                    const int64_t nsec )
-  : mSec(sec)
-  , mNSec(nsec)
+Duration::Duration( const double time )
+  : mTime{static_cast <time_t> ( std::trunc(time) ),
+          static_cast <long> (time * NSEC_IN_SEC) % NSEC_IN_SEC}
+{}
+
+Duration::Duration(
+  const time_t sec,
+  const long nsec )
+  : mTime{sec, nsec}
 {}
 
 bool
@@ -95,64 +83,28 @@ Duration::now()
   return *this = TimeUtils::Now();
 }
 
-#if defined (TIME_UTILS_WIN)
-Duration::Duration( const LARGE_INTEGER& time )
-{
-  LARGE_INTEGER perfFreq;
-  QueryPerformanceFrequency( &perfFreq );
-
-//  Note: Windows has 100 ns time stamp resolution
-  mSec = time.QuadPart / perfFreq.QuadPart;
-  mNSec = (time.QuadPart % perfFreq.QuadPart) * NSEC_IN_SEC / perfFreq.QuadPart;
-}
-
-Duration::operator LARGE_INTEGER () const
-{
-  LARGE_INTEGER perfFreq, value;
-
-  QueryPerformanceFrequency( &perfFreq );
-  value.QuadPart = mSec * perfFreq.QuadPart + mNSec * perfFreq.QuadPart / NSEC_IN_SEC;
-
-  return value;
-}
-
-#elif defined (TIME_UTILS_LIN)
-Duration::Duration( const struct timespec& time )
-{
-  mSec = time.tv_sec;
-  mNSec = time.tv_nsec;
-}
-
 Duration::operator timespec () const
 {
-  timespec value;
-
-  value.tv_sec  = mSec;
-  value.tv_nsec = mNSec;
-
-  return value;
+  return mTime;
 }
-
-#endif
 
 Duration::operator double () const
 {
   return
-    mSec + mNSec / static_cast <double> ( NSEC_IN_SEC );
+    mTime.tv_sec + mTime.tv_nsec / static_cast <double> (NSEC_IN_SEC);
 }
 
 Duration::operator float () const
 {
-  return
-    mSec + mNSec / static_cast <double> ( NSEC_IN_SEC );
+  return static_cast <double> (*this);
 }
 
 bool
 Duration::operator == ( const Duration& other ) const
 {
   return
-    this->mSec == other.mSec &&
-    this->mNSec == other.mNSec;
+    this->mTime.tv_sec == other.mTime.tv_sec &&
+    this->mTime.tv_nsec == other.mTime.tv_nsec;
 }
 
 bool
@@ -164,10 +116,10 @@ Duration::operator != ( const Duration& other ) const
 bool
 Duration::operator < ( const Duration& other ) const
 {
-  if ( this->mSec == other.mSec )
-    return this->mNSec < other.mNSec;
+  if ( this->mTime.tv_sec == other.mTime.tv_sec )
+    return this->mTime.tv_nsec < other.mTime.tv_nsec;
 
-  return this->mSec < other.mSec;
+  return this->mTime.tv_sec < other.mTime.tv_sec;
 }
 
 bool
@@ -193,13 +145,13 @@ Duration::operator + ( const Duration& other ) const
 {
   Duration result;
 
-  result.mSec = this->mSec + other.mSec;
-  result.mNSec = this->mNSec + other.mNSec;
+  result.mTime.tv_sec = this->mTime.tv_sec + other.mTime.tv_sec;
+  result.mTime.tv_nsec = this->mTime.tv_nsec + other.mTime.tv_nsec;
 
-  while ( result.mNSec >= NSEC_IN_SEC )
+  while ( result.mTime.tv_nsec >= NSEC_IN_SEC )
   {
-    ++result.mSec;
-    result.mNSec -= NSEC_IN_SEC;
+    ++result.mTime.tv_sec;
+    result.mTime.tv_nsec -= NSEC_IN_SEC;
   }
 
   return result;
@@ -210,13 +162,13 @@ Duration::operator - ( const Duration& other ) const
 {
   Duration result;
 
-  result.mSec = this->mSec - other.mSec;
-  result.mNSec = this->mNSec - other.mNSec;
+  result.mTime.tv_sec = this->mTime.tv_sec - other.mTime.tv_sec;
+  result.mTime.tv_nsec = this->mTime.tv_nsec - other.mTime.tv_nsec;
 
-  while ( result.mNSec < 0 )
+  while ( result.mTime.tv_nsec < 0 )
   {
-    --result.mSec;
-    result.mNSec += NSEC_IN_SEC;
+    --result.mTime.tv_sec;
+    result.mTime.tv_nsec += NSEC_IN_SEC;
   }
 
   return result;
